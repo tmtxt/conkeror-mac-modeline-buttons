ede35c09f994f3ee48d61009ed26e200159c9a28
tmtxt <tmtxt@txt-mac.local>
2013-09-14 13:07:44 +0700

    change var and func names

--- tmtxt-conkeror-mac-modeline-buttons.js
+++ tmtxt-conkeror-mac-modeline-buttons.js
@@ -1,3 +1,7 @@
+/// Author: Tran Xuan Truong
+/// Webiste: truongtx.me
+/// Email: me@truongtx.me
+
 /// Basic navigation buttons in mode line
 /// include mode-line-button library
 load_paths.unshift("chrome://conkeror-contrib/content/");
@@ -12,34 +16,11 @@ cmmb_image_path.append("tmtxt-conkeror-mac-modeline-buttons");
 cmmb_image_path.append("images");
 cmmb_image_path = make_uri(cmmb_image_path).spec;
 
-/// Function to replace make_button_widget
-function tmtxt_make_button_widget (command, attributes) {
-    if (typeof attributes == "string")
-        // Simple case
-        attributes = { src: cmmb_image_path + attributes + ".png" };
-
-    function new_widget (window) {
-        button_widget.call(this, window);
-    }
-    new_widget.prototype = {
-        constructor: new_widget,
-        __proto__: button_widget.prototype,
-        command: command,
-        attributes: attributes
-    };
-    new_widget.mode_line_adder = function (window) {
-        var widget = new new_widget(window);
-        window.mode_line.add_widget(widget, widget.make_element(window));
-    };
-
-    return new_widget;
-}
-
 /// The array to hold all the mode line buttons
-var tmtxt_mode_line_button_widgets = new Array();
+var cmmb_widgets = new Array();
 
 /// Function to replace the function mode_line_add_buttons
-function tmtxt_mode_line_add_buttons (buttons, prepend) {
+function cmmb_add_buttons (buttons, prepend) {
   for (var i = 0, n = buttons.length; i < n; i++) {
 	
 
@@ -48,20 +29,20 @@ function tmtxt_mode_line_add_buttons (buttons, prepend) {
 
 	// my custom code
 	if(typeof buttons[j][1]  == "string")
-	  buttons[j][1] = { src: cmmb_image_path + buttons[j][1] + ".png" };
+	  buttons[j][1] = { src: "file://" + cmmb_image_path + buttons[j][1] + ".png" };
 
 	/// code from conkeror source file
 	var w = make_button_widget(buttons[j][0], buttons[j][1]);
     add_hook("mode_line_hook", mode_line_adder(w), prepend);
 
 	// My custom code
-	tmtxt_mode_line_button_widgets[i] = w; // add the button to the array
+	cmmb_widgets[i] = w; // add the button to the array
 
   }
 }
 
 /// The array to hold the definition for the mode line buttons
-tmtxt_standard_mode_line_buttons = [
+cmmb_navigation_buttons = [
     ["find-url", "open"],
     ["find-url-new-buffer", "new"],
     ["back", "go-back"],
@@ -73,12 +54,12 @@ tmtxt_standard_mode_line_buttons = [
 ];
 
 /// Interactive function to show the mode line buttons
-interactive("tmtxt-add-mode-line-nav-buttons", "Add basic navigation buttons to the mode line",
+interactive("cmmb-add-navigation-buttons", "Add basic navigation buttons to the mode line",
 			function(I){
 			  // check if the array that hold all the button widgets contains no element
-			  if(tmtxt_mode_line_button_widgets.length == 0){
+			  if(cmmb_widgets.length == 0){
 				// add the button
-				tmtxt_mode_line_add_buttons(tmtxt_standard_mode_line_buttons, true);
+				cmmb_add_buttons(cmmb_navigation_buttons, true);
 				
 				// restart mode line
 				mode_line_mode(false);
@@ -88,15 +69,15 @@ interactive("tmtxt-add-mode-line-nav-buttons", "Add basic navigation buttons to 
 			});
 
 /// Interactive function to hide the mode line buttons
-interactive("tmtxt-remove-mode-line-nav-buttons", "Remove navigation buttons from mode line", 
+interactive("cmmb-remove-navigation-buttons", "Remove navigation buttons from mode line", 
 			function(I){
 			  // loop through the array to remove hook
-			  for(var i=0; i<tmtxt_mode_line_button_widgets.length;i++){
-				remove_hook("mode_line_hook", mode_line_adder(tmtxt_mode_line_button_widgets[i]));
+			  for(var i=0; i<cmmb_widgets.length;i++){
+				remove_hook("mode_line_hook", mode_line_adder(cmmb_widgets[i]));
 			  }
 
 			  // remove all widgets in the array
-			  tmtxt_mode_line_button_widgets.length = 0;
+			  cmmb_widgets.length = 0;
 
 			  // restart mode line
 			  mode_line_mode(false);
