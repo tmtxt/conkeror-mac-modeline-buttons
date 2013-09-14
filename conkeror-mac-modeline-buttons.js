d9560fc5de29d68c118d66263a845399eb17ffd7
tmtxt <tmtxt@txt-mac.local>
2013-09-14 14:27:56 +0700

    function to remove buttons

--- conkeror-mac-modeline-buttons.js
+++ conkeror-mac-modeline-buttons.js
@@ -4,10 +4,15 @@
 /// Version: 1.0
 
 /// Basic navigation buttons in mode line
+
+//////////////////////
 /// include mode-line-button library
 load_paths.unshift("chrome://conkeror-contrib/content/");
 require("mode-line-buttons.js");
 
+
+//////////////////////
+/// Code for adding and removing buttons
 // The variable specifies the path to all the image
 // default is ~/.conkerorrc/conkeror-mac-modeline-buttons/images/
 var cmmb_image_path;
@@ -17,26 +22,34 @@ cmmb_image_path.append("conkeror-mac-modeline-buttons");
 cmmb_image_path.append("images");
 cmmb_image_path = make_uri(cmmb_image_path).spec;
 
-
-
 /// Function to replace the function mode_line_add_buttons
+/// Input
+/// buttons: array of button definition (see more below)
+/// prepend: usually true
+/// widgets_array: the array to hold the widgets
 function cmmb_add_buttons (buttons, prepend, widgets_array) {
-  for (var i = 0, n = buttons.length; i < n; i++) {
-	
-	// code from conkeror sourse file, don't care
-    var j = prepend ? n - i - 1 : i;
+  // check if the widget_arrays has no elements
+  if(widgets_array.length == 0){
+	// loop through the array
+	for (var i = 0, n = buttons.length; i < n; i++) {
+	  
+	  // code from conkeror sourse file, don't care
+      var j = prepend ? n - i - 1 : i;
 
-	// my custom code
-	if(typeof buttons[j][1]  == "string")
-	  buttons[j][1] = { src: "file://" + cmmb_image_path + buttons[j][1] + ".png" };
+	  // my custom code
+	  if(typeof buttons[j][1]  == "string")
+		buttons[j][1] = { src: "file://" + cmmb_image_path + buttons[j][1] + ".png" };
 
-	/// code from conkeror source file, don't care
-	var w = make_button_widget(buttons[j][0], buttons[j][1]);
-    add_hook("mode_line_hook", mode_line_adder(w), prepend);
+	  /// code from conkeror source file, don't care
+	  var w = make_button_widget(buttons[j][0], buttons[j][1]);
+      add_hook("mode_line_hook", mode_line_adder(w), prepend);
 
-	// My custom code
-	widgets_array[i] = w; // add the button to the array
+	  // My custom code
+	  widgets_array[i] = w; // add the button to the array
+	}
 
+	// restart the modeline
+	cmmb_restart_modeline();
   }
 }
 
@@ -47,6 +60,21 @@ function cmmb_restart_modeline(){
   mode_line_mode(true);
 }
 
+/// Function to remove the buttons
+function cmmb_remove_buttons(widgets_array){
+  // loop through the array to remove hook
+  for(var i=0; i<widgets_array.length;i++){
+	remove_hook("mode_line_hook", mode_line_adder(widgets_array[i]));
+  }
+
+  // remove all widgets in the array
+  widgets_array.length = 0;
+
+  // restart the mode line
+  cmmb_restart_modeline();
+}
+
+//////////////////////
 /// Navigation buttons
 /// The array to hold all the mode line navigation button widgets
 var cmmb_navigation_widgets = new Array();
@@ -65,29 +93,16 @@ cmmb_navigation_buttons = [
 /// Interactive function to show the mode line navigation buttons
 interactive("cmmb-add-navigation-buttons", "Add basic navigation buttons to the mode line",
 			function(I){
-			  // check if the array that hold all the button widgets contains no element
-			  if(cmmb_navigation_widgets.length == 0){
-				// add the button
-				cmmb_add_buttons(cmmb_navigation_buttons, true, cmmb_navigation_widgets);
-
-				// restart modeline
-				cmmb_restart_modeline();
-			  }			  
+			  cmmb_add_buttons(cmmb_navigation_buttons, true, cmmb_navigation_widgets);
 			});
 /// Interactive function to hide the mode line navigation buttons
 interactive("cmmb-remove-navigation-buttons", "Remove navigation buttons from mode line", 
 			function(I){
-			  // loop through the array to remove hook
-			  for(var i=0; i<cmmb_navigation_widgets.length;i++){
-				remove_hook("mode_line_hook", mode_line_adder(cmmb_navigation_widgets[i]));
-			  }
-
-			  // remove all widgets in the array
-			  cmmb_navigation_widgets.length = 0;
-
-			  // restart the mode line
-			  cmmb_restart_modeline();
+			  cmmb_remove_buttons(cmmb_navigation_widgets);
 			});
+
+//////////////////////
+
 
 /// provide the library
 provide("conkeror-mac-modeline-buttons");
